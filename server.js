1e293fc8c92ba15639c0d13077be1d76f215e4c3
edim dendy <edim@softwareseni.com>
2019-11-03 07:38:53 +0700

    build crud bootcamp and error handler api

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 // Load env file
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
+// Body parser
+app.use(express.json())
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  // app.use(logger);
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`));
 
-// const server = http.createServer((req, res) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.setHeader('X-Powered-By', 'Node.js');
-//   res.end(
-//     JSON.stringify({
-//       success: true,
-//       data: todos
-//     })
-//   );
-//   res.write('Hello')
-//   res.end()
-// });
 
-app.listen(PORT, () => console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`));
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+})
