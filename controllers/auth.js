1718cb16229a41a02ea5513460adb0471f8da66c
edim dendy <edim@softwareseni.com>
2019-11-11 06:49:02 +0700

    forgot password

--- controllers/auth.js
+++ controllers/auth.js
@@ -90,3 +90,44 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: user
   });
 });
+
+// @desc      Forgot password
+// @route     POST /api/v1/auth/forgotpassword
+// @access    Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+  console.log('resetToken', resetToken)
+  await user.save({ validateBeforeSave: false });
+
+  // // Create reset url
+  // const resetUrl = `${req.protocol}://${req.get(
+  //   'host'
+  // )}/api/v1/auth/resetpassword/${resetToken}`;
+
+  // const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  // try {
+  //   await sendEmail({
+  //     email: user.email,
+  //     subject: 'Password reset token',
+  //     message
+  //   });
+
+    res.status(200).json({ success: true, data: user });
+  // } catch (err) {
+  //   console.log(err);
+  //   user.resetPasswordToken = undefined;
+  //   user.resetPasswordExpire = undefined;
+
+  //   await user.save({ validateBeforeSave: false });
+
+  //   return next(new ErrorResponse('Email could not be sent', 500));
+  // }
+});
